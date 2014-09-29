8bf99e5cd79decf7e1cfec5dc2bc3e304ad33991
Matthew Conquergood <matthew.conquergood@gmail.com>
2014-09-28 19:00:47 -0700

    Almost done, but animation not timed right yet.

--- EetsaRace.js
+++ EetsaRace.js
@@ -1,10 +1,12 @@
 //**
-//-Functions-
+//-Global Functions-
 //**
 
 function getRandomInt(min, max) {   //Makes getting a random integer in a range much easier (stackoverflow.com)
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
 
 //**
 //-Constructor-
@@ -18,13 +20,17 @@ function Racer(name,speed,agility,focus){
 
   this.agility=agility;                     //if random roll is higher than agility, racer will be eaten and lose
 
-  this.focus=focus;                         //if focus<1 then the animlal will piss off
+  this.focus=focus;                         //if focus<1 then the animal will piss off
 
   this.refocusValue=focus;                  //stores the original focus value for resets
 
-  this.position=0;                          //First to exceed 100 winzies
+  this.position=1;                          //First to exceed 100 winzies
 
   this.isStillRacing = true;                //And when someone exceeds 100, this gets set to dentures (falsies)
+
+  this.raceX = function(){
+    return (this.position*7);
+  };
 
   this.hasWon = function(){                 //returns true if the racer is >100 in position, else false
     return (this.position>100);
@@ -43,10 +49,111 @@ function Racer(name,speed,agility,focus){
     this.focus = this.refocusValue;
     this.advance();
   };
-}
+};
                                             //*********************************
 $(document).ready(function(){               //***** BEGIN READY FUNCTION ******
                                             //*********************************
+
+  //**
+  //-Game Function-
+  //**
+
+  function raceTheRacers(){
+
+    $('#tortoise')
+        .css('left', tortoise.raceX());     //set the initial X position (1)
+    $('#hare')
+        .css('left', hare.raceX());         //set the initial X position (1)
+
+    var i=0;                                //I will use this below.
+    var raceStatus = "";
+
+    while(tortoise.isStillRacing && hare.isStillRacing){
+
+      i++;                                  //iteration counter
+
+      //When the tortoise loses focus, he pisses off for only one click
+      //Tortoise focus always starts @33, so only a speed 3 can lose focus, and it's his last turn before winning
+      //So Tortoise with speed 3 idles 1 turn at 99.
+      //The hare would have to be idling @ hare.position of 100-hare.speed to win b/c of hare.speed=3 idle
+      //But a Speed 3 Tortoise will lose to a hare who gets distracted within 1 move of winning.
+
+      if (tortoise.focus > 0) {
+        tortoise.advance();
+        if (i % 2 == 0) {
+          $('#tortoise')
+              .attr('src', tortoise.pic2)
+              .css('left', tortoise.raceX());
+        } else {
+          $('#tortoise')
+              .attr('src', tortoise.pic1)
+              .css('left', tortoise.raceX());
+        }   //switches the tortoise pic back and forth so he gurgles
+      } else if (tortoise.focus === 0) {
+        tortoise.pissOff();
+        $('#tortoise').css('src', tortoise.sleepingPic);
+      } else {
+        tortoise.refocus();
+        if (i % 2 == 0) {
+          $('#tortoise')
+              .attr('src', tortoise.pic2)
+              .css('left', tortoise.raceX());
+        } else {
+          $('#tortoise')
+              .attr('src', tortoise.pic1)
+              .css('left', tortoise.raceX());
+        }   //switches the tortoise pic back and forth so he gurgles
+      }
+
+      //For the hare, he advances until hitting 0 focus
+      //He then continues to piss off until the tortoise.position>=hare.position (the tortoise catches up to him)
+
+      if (hare.focus>0){
+        hare.advance();
+        $('#hare').css('left', hare.raceX());
+      } else if ((hare.focus<0) && (tortoise.position>=hare.position)) {
+        hare.refocus();
+        $('#hare').attr('src', hare.pic).css('left', hare.raceX());
+      } else {
+        hare.pissOff();
+        $('#hare').attr('src', hare.sleepingPic);
+      }
+
+      //After they move check to see if either or both will have passed the finish line if they don't get injured.
+
+      if (hare.hasWon() && tortoise.hasWon()){
+        raceStatus = "It's a tie!!";
+        $('#finishLine').attr('src', 'img/finishedLine.png');
+        hare.isStillRacing = false;
+      } else if (hare.hasWon()){
+        $('#finishLine').attr('src', 'img/finishedLine.png');
+        raceStatus = hare.name + " has won the race!  Congratulations!!";
+        hare.isStillRacing = false;
+      } else if (tortoise.hasWon()){
+        $('#finishLine').attr('src', 'img/finishedLine.png');
+        raceStatus = "Gurgle has won the race!  Better luck next time!  Fortunately rabbits breed like... well, rabbits.\n" +
+            "You'll have a whole new batch to choose from next time.";
+        tortoise.isStillRacing = false;
+      }
+
+      //The last thing to check is whether one of the racers is dropping out due to injury.
+      //(This will override current move.)
+
+      if (getRandomInt(0,99)>tortoise.agility){
+        raceStatus = "Gurgle was captured by Timothy Anderson and turned into turtle soup.\n"
+            + hare.name + " wins!";
+        $('#tortoise').attr('src', tortoise.eatenPic);
+        tortoise.isStillRacing = false;
+      } else if (getRandomInt(0,99)>hare.agility) {
+        raceStatus = hare.name + " was eaten by Hank, Peggy and Bobby.  Rednecks will eat anything.\n Gurgle wins!";
+        $('#hare').attr('src', hare.eatenPic);
+        hare.isStillRacing = false;
+      }
+
+    }
+
+  }
+
 
   //**
   //-Variables-
@@ -56,14 +163,6 @@ $(document).ready(function(){               //***** BEGIN READY FUNCTION ******
   var rabbit = [];                          //there will be a bunch of rabbits, of course
   var tortoise;                             //there will eventually be a tortoise
   var hare;
-  var vpHeight = $('#background>img')
-      .height();                            //viewport height
-  var vpWidth = $('#background>img')
-      .width();                             //viewport width
-
-  var startingX = (vpWidth *.05);           //Race Starting X
-  var finishX = (vpWidth *.95);             //Race Finish Line X
-  var raceY =  (vpHeight *.60);             //Race Y
 
   var picLetter = ['A','B','C','D','E'];    //Because my pics were named this way
 
@@ -148,48 +247,48 @@ $(document).ready(function(){               //***** BEGIN READY FUNCTION ******
       + tortoise.agility);                  //Update tortoise agility on the menu
 
 
-  //**
-  //-The Menu-
-  //**
-
-  $('.bunnyBox')
-      .mouseenter(function(event){              //Whenever we put the mouse into a bunnybox div
-        var moused = event.target.id;           //Gives the id of the div into which we've moused (conveniently 0-4)
-        if (picLetter[moused] !== undefined){   //Sometimes I get an undefined value here, so whatever
-          $('#' + moused)
-              .attr('src',
-              rabbit[moused].pic);              //apply image src
-        }
-      })     //Wake up Bunny!
-      .mouseleave(function(event) {             //Whenever we move the mouse out of a bunnybox
-        var moused = event.target.id;           //Gives the id of the div out of which we've moused (conveniently 0-4)
-        if (picLetter[moused] !== undefined) {  //Sometimes I get an undefined value here, so whatever
-          $('#' + moused)
-              .attr('src',
-              rabbit[moused].sleepingPic);      //apply image src
-        }
-      });   //Sleepy time Bunny!
-
-
-  //**
-  //-The Game-
-  //**
-
-  $('.wabbit')                              //When we cwick on da wabbit, we pway.
+  $('.wabbit')                              //Click a rabbit to play
       .click(function(event){
-        var picked = event.target.id;       //Tells us the rabbit we picked, 0-4
+        var pickedID = event.target.id;     //Tells us the rabbit we picked
+        var picked = pickedID.charAt(2);    //Takes the 3rd character since I shouldn't use numbers as ID's.
+
         hare = rabbit[picked];              //Uses that number to select the right object
 
         $("#menu")                          //Selects menu objects
             .hide("slow");                  //Hides the menu objects
 
+        $('#hare')
+            .attr('src', hare.pic );
+
         $('#game')                          //Selects the game objects
             .delay(500)                     //Waits 1/2 second
-            .show("slow");                  //Show's the game objects
+            .show("slow")
+            .delay(1000);
 
+        raceTheRacers();
 
+      })
+      .mouseenter(function(event){              //Whenever we put the mouse into a bunnybox div
+        var mousedID = event.target.id;         //Gives the id of the div into which we've moused (conveniently 0-4)
+        var moused = mousedID.charAt(2);
+        if (picLetter[moused] !== undefined){   //Sometimes I get an undefined value here, so whatever
+          $('#bb' + moused)
+              .attr('src',
+              rabbit[moused].pic);              //apply image src
+        }
 
-      });
+      })     //Wake up Bunny!
+      .mouseleave(function(event) {             //Whenever we move the mouse out of a bunnybox
+        var mousedID = event.target.id;         //Gives the id of the div out of which we've moused (conveniently 0-4)
+        var moused = mousedID.charAt(2);
+        if (picLetter[moused] !== undefined) {  //Sometimes I get an undefined value here, so whatever
+          $('#bb' + moused)
+              .attr('src',
+              rabbit[moused].sleepingPic);      //apply image src
+        }
+
+      });   //Sleepy time Bunny!;
+
 
 
 
@@ -201,114 +300,6 @@ $(document).ready(function(){               //***** BEGIN READY FUNCTION ******
                                             //*********************************
 
 
-
-
-/*
- $('#path')
- .width(vpWidth+'px')
- .height(vpHeight+'px')
- .css('display','inline');
-
- $('#grass')
- .width(vpWidth+'px')
- .height((vpHeight*.3)+'px');
-
- $('#hare')
- .width(rWidth)
- .height(rHeight)
- .css('left', rPosX + 'px')
- .css('bottom', rPosY + 'px')
- .css('display','inline');
-
-
-bunnyReport += ( (i+1) + " - " + rabbit[i].name +
-      "  | Speed: " + rabbit[i].speed +
-      "  | Agility: " + rabbit[i].agility +
-      "  | Focus: " + rabbit[i].focus + "\n")
-}
-
-
-//Let the player pick a rabbit
-
-var selection = prompt("The Tortoise you are racing is named " + tortoise.name + "\n" +
-    "  His stats are as follows: \n" +
-    "Speed: " + tortoise.speed +
-    "  | Agility: " + tortoise.agility +
-    "  | Focus: " + tortoise.focus + "\n" +
-    "You may select from the following racers:\n" +
-    bunnyReport + "Please enter 1-5 to make your selection:");
-
-if (typeof (selection) === 'string'){selection=parseInt(selection);} //Thank you, Sam.
-var hare = rabbit[selection-1]; //because array
-
-//Race the tortoise and the hare
-
-var raceStatus = "The race continues!";
-while(tortoise.isStillRacing && hare.isStillRacing){
-
-  //When the tortoise loses focus, he pisses off for only one click
-  //Tortoise focus always starts @33, so only a speed 3 can lose focus, and it's his last turn before winning
-  //So Tortoise with speed 3 idles 1 turn at 99.
-  //The hare would have to be idling @ hare.position of 100-hare.speed to win b/c of hare.speed=3 idle
-  //But a Speed 3 Tortoise will lose to a hare who gets distracted within 1 move of winning.
-
-  if (tortoise.focus > 0) {
-    tortoise.advance();
-  } else if (tortoise.focus === 0) {
-    tortoise.pissOff();
-  } else {
-    tortoise.refocus();
-  }
-
-  //For the hare, he advances until hitting 0 focus
-  //He then continues to piss off until the tortoise.position>=hare.position (the tortoise catches up to him)
-
-  if (hare.focus>0){
-    hare.advance();
-  } else if ((hare.focus<0) && (tortoise.position>=hare.position)) {
-    hare.refocus();
-  } else {
-    hare.pissOff();
-  }
-
-  //After they move check to see if either or both will have passed the finish line if they don't get injured.
-
-  if (hare.hasWon() && tortoise.hasWon()){
-    raceStatus = "It's a tie!";
-    hare.isStillRacing = false;
-  } else if (hare.hasWon()){
-    raceStatus = hare.name + " has won the race!  Congratulations!!";
-    hare.isStillRacing = false;
-  } else if (tortoise.hasWon()){
-    raceStatus = "Gurgle has won the race!  Better luck next time!  Fortunately rabbits breed like... well, rabbits.\n" +
-        "You'll have a whole new batch to choose from next time.";
-    tortoise.isStillRacing = false;
-  }
-
-  //The last thing to check is whether one of the racers is dropping out due to injury.
-  //(This will override current move.)
-
-  if (getRandomInt(0,99)>tortoise.agility){
-    tortoise.statusMessage = "Gurgle was captured by Timothy Anderson and turned into turtle soup.\n";
-    raceStatus = hare.name + " wins!";
-    tortoise.isStillRacing = false;
-  } else if (getRandomInt(0,99)>hare.agility) {
-    hare.statusMessage = hare.name + " was eaten by Hank, Peggy and Bobby.  Rednecks will eat anything.\n";
-    raceStatus = "Gurgle wins!";
-    hare.isStillRacing = false;
-  }
-
-  //Finally, build an alert to notify the status of the round.
-
-  var report = hare.statusMessage + "\n" + tortoise.statusMessage + "\n" + raceStatus;
-
-  //and report the results
-
-  alert(report);
-
-}
-*/
-
 /**
- * Created by evilOlive on 9/20/2014.
+ * Created by evilOlive on 9/28/2014.
  */
